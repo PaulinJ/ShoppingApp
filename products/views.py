807bb19f857b993f52986b5ce4937298ec9e819e
Paulin Junior <cingombapaulin@gmail.com>
2025-04-26 01:25:26 +0200

    Create Options and add Products

--- products/views.py
+++ products/views.py
@@ -1,6 +1,24 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from .models import Product
 
 def index(request):
-    return HttpResponse('Hi There')
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})
 
+def add_to_cart(request, product_id):
+    cart = request.session.get('cart', {})
+    cart[product_id] = cart.get(product_id, 0) + 1
+    request.session['cart'] = cart
+    return redirect('/cart/')
+
+def cart(request):
+    cart = request.session.get('cart', {})
+    products = Product.objects.filter(id__in=cart.keys())
+    cart_items = []
+    for product in products:
+        cart_items.append({
+            'product': product,
+            'quantity': cart[str(product.id)]
+        })
+    return render(request, 'cart.html', {'cart_items': cart_items})
